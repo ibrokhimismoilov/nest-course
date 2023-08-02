1d18b0dd909026aa92612af06a525dcc2f9e6d39
ibrokhim <mr017programmer@gmail.com>
2023-08-02 08:11:13 +0500

    Connected postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { ConfigModule } from '@nestjs/config';
     SequelizeModule.forRoot({
       dialect: 'postgres',
       host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
+      port: +process.env.POSTGRES_PORT,
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
